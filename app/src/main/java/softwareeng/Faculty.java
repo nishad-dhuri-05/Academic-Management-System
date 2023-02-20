8ff0fe4666643425530949a8914d1c0f891c30da
Subham_subhasis <subhamsubhasis2002@gmail.com>
2023-02-20 18:03:42 +0530

    upadte faculty profile func

--- app/src/main/java/softwareeng/Faculty.java
+++ app/src/main/java/softwareeng/Faculty.java
@@ -20,7 +20,8 @@ public class Faculty {
             System.out.println("3. View All Course Offerings");
             System.out.println("4. Upload Grades");
             System.out.println("5. View Grades");
-            System.out.println("6. Logout");
+            System.out.println("6. Update Profile");
+            System.out.println("7. Logout");
 
             int option = 0;
 
@@ -39,6 +40,8 @@ public class Faculty {
             } else if (option == 5) {
                 view_grades();
             } else if (option == 6) {
+                update_profile();
+            } else if (option == 7) {
                 return;
             } else {
                 System.out.println("Select a valid option \n");
@@ -288,6 +291,95 @@ public class Faculty {
                 System.out.println("  " + entry_no + "  " + "|" + course_code + "|" + grade + "|" + status);
 
             }
+        }
+
+    }
+
+    public static void update_profile() throws Exception {
+        ResourceBundle rd = ResourceBundle.getBundle("config");
+        String url = rd.getString("url"); // localhost:5432
+        String username = rd.getString("username");
+        String password = rd.getString("password");
+
+        Class.forName("org.postgresql.Driver");
+        Connection con = DriverManager.getConnection(url, username, password);
+
+        while (true) {
+
+            String email_field = "email";
+            String name_field = "name";
+            String department_field = "department";
+            String password_field = "password";
+            String joining_data_field = "joining_date";
+
+            System.out.println(" \n Select field to update : ");
+            System.out.println("1. " + email_field);
+            System.out.println("2. " + name_field);
+            System.out.println("3. " + department_field);
+            System.out.println("4. " + password_field);
+            System.out.println("5. " + joining_data_field);
+            System.out.println("6. Go Back ");
+
+            int option = 0;
+            Scanner sc = new Scanner(System.in);
+            option = sc.nextInt();
+            sc.nextLine();
+
+            String email = "";
+            String query = "";
+            Statement st;
+            ResultSet rs;
+            int x;
+
+            query = "Select email from logs;";
+
+            st = con.createStatement();
+            rs = st.executeQuery(query);
+
+            while (rs.next()) {
+                email = rs.getString("email");
+            }
+
+            if (option == 1) {
+                System.out.println("Enter new " + email_field);
+                String new_email = sc.nextLine();
+
+                query = String.format("update auth set email = '%s' where email = '%s' ", new_email, email);
+                st = con.createStatement();
+                x = st.executeUpdate(query);
+
+            } else if (option == 2) {
+                System.out.println("Enter new " + name_field);
+                String new_name = sc.nextLine();
+
+                query = String.format("update auth set name = '%s' where email = '%s'", new_name, email);
+                st = con.createStatement();
+                x = st.executeUpdate(query);
+            } else if (option == 3) {
+                System.out.println("Enter new " + department_field);
+                String new_dept = sc.nextLine();
+
+                query = String.format("update auth set department = '%s' where email = '%s'", new_dept, email);
+                st = con.createStatement();
+                x = st.executeUpdate(query);
+            } else if (option == 4) {
+                System.out.println("Enter new " + password_field);
+                String new_pass = sc.nextLine();
+
+                query = String.format("update auth set password = '%s' where email = '%s' ", new_pass, email);
+                st = con.createStatement();
+                x = st.executeUpdate(query);
+            } else if (option == 5) {
+                System.out.println("Enter new " + joining_data_field + " ( Format : YYYY-MM-DD)");
+                String new_doj = sc.nextLine();
+
+                query = String.format("update auth set joining_date = '%s' where email = '%s' ", new_doj, email);
+                st = con.createStatement();
+                x = st.executeUpdate(query);
+            } else {
+                break;
+            }
+
         }
 
     }
